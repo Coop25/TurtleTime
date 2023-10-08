a84d36050de79c315f39be70b42df5f78e51271b
Cooper <frankidyfrank3@Gmail.com>
2023-10-08 14:21:15 -0500

    added deleting of old files

--- startup.lua
+++ startup.lua
@@ -1,3 +1,6 @@
+shell.run("delete", "stairs.lua")
 shell.run("wget", "https://raw.githubusercontent.com/Coop25/TurtleTime/master/stairs.lua", "stairs.lua")
+shell.run("delete", "utils.lua")
 shell.run("wget", "https://raw.githubusercontent.com/Coop25/TurtleTime/master/utils/utils.lua", "utils.lua")
+shell.run("delete", "startup.lua")
 shell.run("wget", "https://raw.githubusercontent.com/Coop25/TurtleTime/master/startup.lua", "startup.lua")
