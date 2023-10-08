f8357a7565b33fe96f8c2bdc3db3e34317dde1a5
Cooper <frankidyfrank3@Gmail.com>
2023-10-08 14:11:10 -0500

    created startup

--- startup.lua
+++ startup.lua
@@ -1,2 +1,3 @@
 shell.run("wget", "https://raw.githubusercontent.com/Coop25/TurtleTime/master/stairs.lua", "stairs.lua")
 shell.run("wget", "https://raw.githubusercontent.com/Coop25/TurtleTime/master/utils/utils.lua", "utils.lua")
+shell.run("wget", "https://raw.githubusercontent.com/Coop25/TurtleTime/master/startup.lua", "startup.lua")
