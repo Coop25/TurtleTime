4cade224a1e0077b7b0e5494584fa2089607cf20
Cooper <frankidyfrank3@Gmail.com>
2023-10-11 12:55:05 -0500

    added falling block prevention

--- stairs.lua
+++ stairs.lua
@@ -4,7 +4,9 @@ print("What Y level is your head?: ")
 local yDistance = tonumber(io.read()) + 50
 print("How often do you want a torch?: ")
 local torchDistance = tonumber(io.read())
-turtle.dig()
+while turtle.detect() do
+    turtle.dig()
+end
 turtle.forward()
 
 function emptyInv()
@@ -24,24 +26,36 @@ end
 for i = 1, yDistance do
     for i = 1, 4 do
         turtle.turnLeft()
-        turtle.dig()
+        while turtle.detect() do
+            turtle.dig()
+        end
         turtle.turnRight()
         turtle.turnRight()
-        turtle.dig()
+        while turtle.detect() do
+            turtle.dig()
+        end 
         turtle.turnLeft()
         if i <= 3 then
-            turtle.dig()
+            while turtle.detect() do
+                turtle.dig()
+            end
             turtle.forward()
         end
     end
     if i % torchDistance == 0 then
-        turtle.dig()
+        while turtle.detect() do
+            turtle.dig()
+        end
         turtle.forward()
         turtle.turnLeft()
-        turtle.dig()
+        while turtle.detect() do
+            turtle.dig()
+        end
         turtle.turnRight()
         turtle.turnRight()
-        turtle.dig()
+        while turtle.detect() do
+            turtle.dig()
+        end
         turtle.turnLeft()
         turtle.back()
         utils.awaitSelectItem("minecraft:torch")
